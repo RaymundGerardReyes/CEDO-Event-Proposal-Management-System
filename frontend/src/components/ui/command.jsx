d21a32b19199066a8ad836720964fe7144626236
RaymundGerardEstaca <2022025162@my.xu.edu.ph>
2025-06-08 14:12:45 +0800

    feat(ui): enhance command UI component

--- frontend/src/components/ui/command.jsx
+++ frontend/src/components/ui/command.jsx
@@ -1,13 +1,14 @@
 // components/ui/command.jsx
 "use client"
 
-import * as React from "react"
+import * as React from "react";
 // Removed: import { type DialogProps } from "@radix-ui/react-dialog"
-import { Command as CommandPrimitive } from "cmdk"
-import { Search } from "lucide-react"
+import { Command as CommandPrimitive } from "cmdk";
+import { Search } from "lucide-react";
 
-import { Dialog, DialogContent } from "@/components/ui/dialog"; // Assuming dialog is already JS/JSX
-import { cn } from "@/lib/utils"
+import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog"; // Assuming dialog is already JS/JSX
+import { VisuallyHidden } from "@/components/ui/visually-hidden";
+import { cn } from "@/lib/utils";
 
 const Command = React.forwardRef( // Removed generic type parameters
   ({ className, ...props }, ref) => (
@@ -26,6 +27,9 @@ const CommandDialog = ({ children, ...props }) => { // Removed DialogProps type
   return (
     <Dialog {...props}>
       <DialogContent className="overflow-hidden p-0 shadow-lg">
+        <VisuallyHidden>
+          <DialogTitle>Command Menu</DialogTitle>
+        </VisuallyHidden>
         <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
           {children}
         </Command>
@@ -131,4 +135,5 @@ export {
   Command,
   CommandDialog, CommandEmpty,
   CommandGroup, CommandInput, CommandItem, CommandList, CommandSeparator, CommandShortcut
-}
+};
+
