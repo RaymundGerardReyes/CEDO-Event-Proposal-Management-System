0c9f1a71d3e1a8c1eb42389e5dc25d15f5f2ee26
RaymundGerardEstaca <2022025162@my.xu.edu.ph>
2025-06-08 14:13:44 +0800

    refactor(app): improve app component state management

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,15 +1,20 @@
-import '@/app/globals.css';
-import { Toaster, ToastProvider } from "@/components/ui/use-toast";
+import { ThemeProvider } from '@/components/theme-provider';
+import { Toaster } from '@/components/ui/toaster';
+import { ToastProvider } from '@/components/ui/use-toast';
 import { AuthProvider } from '@/contexts/auth-context';
+import '../app/globals.css'; // Use the correct path for your global styles
 
 function MyApp({ Component, pageProps }) {
     return (
-        <ToastProvider>
-            <AuthProvider>
-                <Component {...pageProps} />
-                <Toaster />
-            </AuthProvider>
-        </ToastProvider>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            <ToastProvider>
+                <AuthProvider>
+                    <Component {...pageProps} />
+                    <Toaster />
+                </AuthProvider>
+            </ToastProvider>
+        </ThemeProvider>
     );
 }
+
 export default MyApp;
